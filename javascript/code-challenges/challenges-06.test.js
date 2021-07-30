08f787aa2d94c1cd2c53e5aa5e1dfbba8ff22407
don <bandy@gmail.com>
2021-07-29 20:58:02 -0700

    completed 5 stretch goal

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -79,6 +79,8 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
+  return obj;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -96,6 +98,8 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+  people.map(people => people.isAuthor = true);
+  return people;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -164,7 +168,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
